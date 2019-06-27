d311604fd245e7f1790c6ea06927ef5febc50bb9
pradipSodha <38727925+pradipSodha@users.noreply.github.com>
2019-06-27 22:21:03 +0530

    swaping
    issue with logic

---
+++ swap.java
@@ -0,0 +1,19 @@
+public class Main{
+  public static void main(String args[]){
+    int x=2;
+    int y=3;
+    
+    System.out.println("Before swaping :  ");
+    System.out.println("x is "+x);
+    System.out.println("y is "+y);
+    
+    //logic 
+    x = y;
+    y = x;
+    
+    System.out.println("After swaping :  ");
+    System.out.println("x is "+x);
+    System.out.println("y is "+y);
+    
+  }
+}
